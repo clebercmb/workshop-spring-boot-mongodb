57df0fe4685acca74e23ab78a01922cb3492570c
Cleber Miranda Barbosa <cleber@gmail.com>
2019-02-09 20:03:17 -0200

    Inserting user with post

--- src/main/java/com/clebercmb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/clebercmb/workshopmongo/resources/UserResource.java
@@ -1,15 +1,18 @@
 package com.clebercmb.workshopmongo.resources;
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.clebercmb.workshopmongo.dto.UserDTO;
 import com.clebercmb.workshopmongo.services.UserService;
@@ -22,7 +25,7 @@ public class UserResource {
 	@Autowired
 	private UserService service;  // Injection service
 	
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(method=RequestMethod.GET)  //End point path to get all  users.  default path = /users
 	public ResponseEntity<List<UserDTO>> findAll() {
 		
 		var userList = service.findAll(); 
@@ -31,12 +34,26 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) //End point path to get user by id. Path = /users/id
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		
 		var user = service.findById(id);
 				
 		return ResponseEntity.ok().body(new UserDTO(user));
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		var user = service.userFromDTO(objDto);
+		user = service.insert(user);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		System.out.println(">>>" + uri);
+		
+		return ResponseEntity.created(uri).build(); //ResponseEntity.created(uri) will return an empty answer with the code 201
+													//and a header having the location of the new resource created
+														
+	}	
 	
 }
--- src/main/java/com/clebercmb/workshopmongo/services/UserService.java
+++ src/main/java/com/clebercmb/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.clebercmb.workshopmongo.domain.User;
+import com.clebercmb.workshopmongo.dto.UserDTO;
 import com.clebercmb.workshopmongo.repository.UserRepository;
 import com.clebercmb.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,7 +24,15 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		
-		System.out.println(">>>>"+obj);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public User userFromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
 }
