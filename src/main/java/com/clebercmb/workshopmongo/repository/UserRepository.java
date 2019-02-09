ff1693a7b120453fb464050fe8873f746a88735c
Cleber Miranda Barbosa <cleber@gmail.com>
2019-02-09 17:02:35 -0200

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/clebercmb/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.clebercmb.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.clebercmb.workshopmongo.domain.User;
+
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+}
